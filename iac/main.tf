bb4a20b4aaf92021071b415de1e657ea5d89f244
Ricky <rickymbru@gmail.com>
2023-06-11 21:30:19 -0300

    Usando Ansible e Terraform

--- iac/main.tf
+++ iac/main.tf
@@ -17,15 +17,6 @@ resource "aws_instance" "app_server" {
   ami           = "ami-03f65b8614a860c29"
   instance_type = "t2.micro"
   key_name = "alura-iac"
-  user_data = <<-EOF
-                  #!/bin/bash
-                  sudo apt update 
-                  echo "Y" | sudo apt install --yes nginx
-                  sleep 5
-                  cd /var/www/html/
-                  echo "<h1> Feito com Terraform</h1>" | sudo tee index.html
-                  sudo systemctl reload nginx
-                 EOF
   tags = {
     Name = "Teste servidor WEB"
   }
